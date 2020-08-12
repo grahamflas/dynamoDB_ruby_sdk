338b633d70a2cd51b3615f7358b5156e503ea764
Graham Flaspoehler <grahamflas@gmail.com>
2020-08-12 11:54:17 -0400

    Conditionally update an item

---
+++ MoviesItemOps05.rb
@@ -0,0 +1,42 @@
+require "aws-sdk"
+
+Aws.config.update({
+  region: "us-west-2",
+  endpoint: "http://localhost:8000"
+})
+
+dynamodb = Aws::DynamoDB::Client.new
+
+table_name = "Movies"
+
+year = 2015
+title = "The Big New Movie"
+
+params = {
+  table_name: table_name,
+  key: {
+    year: year,
+    title: title
+  },
+  update_expression: "remove info.actors[0]",
+  condition_expression: "size(info.actors) >= :num",
+  expression_attribute_values: {
+    ":num" => 3
+  },
+  return_values: "UPDATED_NEW"
+}
+
+begin
+  result = dynamodb.update_item(params)
+  puts "updated item. ReturnValues are:"
+  result.attributes["info"].each do |key, value|
+    if key == "rating"
+      puts "#{key}: #{value.to_f}"
+    else
+      puts "#{key}: #{value}"
+    end
+  end
+rescue Aws::DynamoDB::Errors::ServiceError => error
+  puts "Unable to update item:"
+  puts "#{error.message}"
+end
